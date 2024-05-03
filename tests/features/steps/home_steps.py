031aae62efd5ef79c92f1703f0fa6921de12201d
floresnes <nestor.flores@storicard.com>
2024-05-03 13:14:45 -0300

    fixes

--- tests/features/steps/home_steps.py
+++ tests/features/steps/home_steps.py
@@ -4,13 +4,13 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 
-@step('The user opens {url}')
-def step_impl(context, url):
+@step('The {actor} opens {url}')
+def step_impl(context, actor, url):
     context.driver.get(url)
 
 
-@step('Then user sees "{text}" title')
-def step_impl(context, text):
+@step('Then {actor} sees "{text}" title')
+def step_impl(context, actor, text):
     title_locator = (By.CSS_SELECTOR, ".post-2715 > div:nth-of-type(1) h2")
     title_element = WebDriverWait(context.driver, 10).until(EC.visibility_of_element_located(title_locator))
     assert title_element.text == "Automate Selenium/Protractor Automation  Scripts"
